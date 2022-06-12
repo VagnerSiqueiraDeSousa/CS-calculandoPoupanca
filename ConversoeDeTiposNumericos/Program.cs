d629b24f25bd9aa351e8f9bf13461c1cab346956
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 18:47:38 -0300

    Add files via upload
    conversoes tipos numericos

---
+++ ConversoeDeTiposNumericos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversoeDeTiposNumericos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oi Vagner, vamos praticar um pouco de tipos numeros");
+
+            float pontoFlutuante = 3.14f;
+            Console.WriteLine(pontoFlutuante);
+
+            double salario = 5270.50;
+            int valor = (int) salario;
+            Console.WriteLine(valor);
+
+            double valor1 = 0.2;
+            double valor2 = 0.1;
+            double total = valor1 + valor2;
+            Console.WriteLine(total);
+
+            int idade = 3 * 10;
+            Console.WriteLine(idade);
+
+
+            Console.WriteLine("Fim do Programa pressione enter para Sair ...");
+            Console.ReadLine();
+        }
+    }
+}
